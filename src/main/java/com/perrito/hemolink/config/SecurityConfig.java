832e231388b8ccca5cfff1f48ea7421798657d25
sabrina mendonça <109186458+sabrainas@users.noreply.github.com>
2025-06-01 15:58:50 -0300

    Update SecurityConfig.java

--- src/main/java/com/perrito/hemolink/config/SecurityConfig.java
+++ src/main/java/com/perrito/hemolink/config/SecurityConfig.java
@@ -10,6 +10,7 @@ import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.CorsConfigurationSource;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import java.util.List;
 
 @Configuration
 @EnableWebSecurity
@@ -42,7 +43,7 @@ public class SecurityConfig {
 	        "http://localhost:5173",
 	        "http://localhost:3000"
     	));
-    	configuration.setAllowedMethods(List.of("*"));
+        configuration.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS"));
     	configuration.setAllowedHeaders(List.of("*"));
     	configuration.setAllowCredentials(true);
 
