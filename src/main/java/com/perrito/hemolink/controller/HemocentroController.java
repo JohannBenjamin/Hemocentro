c0292e677b5f88854ef8823b711a15b85777d5b2
Johann Benjamin <johannduranticona12@gmail.com>
2025-06-02 22:30:27 -0300

    Endpoints do hemocentro(registros de sangue) e do hospital

--- src/main/java/com/perrito/hemolink/controller/HemocentroController.java
+++ src/main/java/com/perrito/hemolink/controller/HemocentroController.java
@@ -1,9 +1,17 @@
 package com.perrito.hemolink.controller;
 
+import java.util.Date;
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.perrito.hemolink.model.entity.Hemocentro;
 import com.perrito.hemolink.service.HemocentroService;
 
 @RestController
@@ -12,5 +20,34 @@ public class HemocentroController {
 	@Autowired
 	private HemocentroService hemocentroService;
 	
-	//public List
+	@GetMapping
+	public List<Hemocentro> getAllHemocentro() {
+		return hemocentroService.getAllHemocentro();
+	}
+		
+	@GetMapping("/{codigo}")
+	public ResponseEntity<Hemocentro> getHemocentroByCodigo(@PathVariable int codigo) {
+		Hemocentro hemocentro = hemocentroService.getHemocentroById(codigo);
+		if (hemocentro != null) {
+			return new ResponseEntity<>(hemocentro, HttpStatus.OK);
+		} else {
+			return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+		}
+	}
+	
+	@GetMapping("/{data}")
+	public List<Hemocentro> getHemocentroByData(@PathVariable Date data) {
+        return hemocentroService.getHemocentroByData(data);
+    }
+		
+	@GetMapping("/{tipo}")
+	public ResponseEntity<Hemocentro> getHemocentroByTipoDesc(@PathVariable String tipo) {
+		Hemocentro hemocentro = hemocentroService.getHemocentroByTipoDesc(tipo);
+		if (hemocentro != null) {
+			return new ResponseEntity<>(hemocentro, HttpStatus.OK);
+		} else {
+			return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+		}
+	}
+		
 }
--- src/main/java/com/perrito/hemolink/controller/HospitalController.java
+++ src/main/java/com/perrito/hemolink/controller/HospitalController.java
@@ -1,9 +1,16 @@
 package com.perrito.hemolink.controller;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.perrito.hemolink.model.entity.Hospital;
 import com.perrito.hemolink.service.HospitalService;
 
 @RestController
@@ -12,5 +19,18 @@ public class HospitalController {
 	@Autowired
 	private HospitalService hospitalService;
 	
+	@GetMapping
+	public List<Hospital> getAllHospital() {
+		return hospitalService.getAllHospital();
+	}
 	
+	@GetMapping("/{codigo}")
+	public ResponseEntity<Hospital> getHospitalById(@PathVariable int codigo) {
+        Hospital hospital = hospitalService.getHospitalById(codigo);
+		if (hospital != null) {
+			return new ResponseEntity<>(hospital, HttpStatus.OK);
+		} else {
+			return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+		}
+    }
 }
