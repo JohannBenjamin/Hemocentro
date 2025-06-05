67b608b6a1a5fdcfb3ca3f3fa1cad58efd2a52a9
Johann Benjamin <johannduranticona12@gmail.com>
2025-06-05 00:20:50 -0300

    teste apagar requisicoes

--- src/main/java/com/perrito/hemolink/service/RequisicaoService.java
+++ src/main/java/com/perrito/hemolink/service/RequisicaoService.java
@@ -51,6 +51,6 @@ public class RequisicaoService {
     }
     
     public void deletarRequisicoesInvalidadas() {
-        requisicaoRepo.deleteByTipoIsNullOrTipo("");
+        requisicaoRepo.deleteAll();
     }
 }
